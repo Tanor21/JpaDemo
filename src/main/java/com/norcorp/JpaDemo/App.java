f0355bc7b008f9ff6bd8c584e185ea2282f49b2b
Ousmane cisse <cisseousmanetanor@gmail.com>
2024-05-15 19:22:46 +0100

    Java : JPA Configure and Retrieve Data From Database

--- src/main/java/com/norcorp/JpaDemo/App.java
+++ src/main/java/com/norcorp/JpaDemo/App.java
@@ -1,13 +1,22 @@
 package com.norcorp.JpaDemo;
 
-/**
- * Hello world!
- *
- */
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       
+    	EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
+    	EntityManager em = emf.createEntityManager();
+    	
+    	Alien a = em.find(Alien.class, 4);
+    	
+    	System.out.println(a);
+    	
     }
 }
