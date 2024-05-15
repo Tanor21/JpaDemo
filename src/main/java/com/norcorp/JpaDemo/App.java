5c7fa256c6f6341273b3ba1f90adf38e3ac9811b
Ousmane cisse <cisseousmanetanor@gmail.com>
2024-05-15 19:53:44 +0100

    Java : JPA save data to DB

--- src/main/java/com/norcorp/JpaDemo/App.java
+++ src/main/java/com/norcorp/JpaDemo/App.java
@@ -10,11 +10,22 @@ public class App
 {
     public static void main( String[] args )
     {
+    	
+    	Alien a = new Alien();
+    	a.setAid(4);
+    	a.setAname("youssef");
+    	a.setAtech("React");
        
     	EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
     	EntityManager em = emf.createEntityManager();
     	
-    	Alien a = em.find(Alien.class, 4);
+    	//Alien a = em.find(Alien.class, 4); //NB: To retrieve data from DB
+    	
+    	em.getTransaction().begin();
+    	
+    	em.persist(a);
+    	
+    	em.getTransaction().commit();
     	
     	System.out.println(a);
     	
